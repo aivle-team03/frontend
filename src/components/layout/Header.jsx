57bcd512fdd88515bc326ae3ec3e9354c36d2c55
hanycorea-commits <hanycorea@gmail.com>
2026-07-13 13:38:54 +0900

    마이페이지 jsx,css파일 생성, Header.css 생성, 헤더에 알림, 프로필 버튼 추가 후 프로필 드롭다운(마이페이지, 로그아웃) 생성

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,37 @@
+import React, { useState } from "react";
+import "../../styles/Header.css";
+
 function Header({ title }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="app-header">
       <h1>{title}</h1>
+      <div className="header-icons">
+        <button className="icon-btn">🔔</button>
+        {/* icon은 추후에 react-icons로 교체 예정 */}
+
+        <div className="profile-menu">
+          <button 
+          className="icon-btn"
+          onClick={() => setIsOpen(!isOpen)}
+          // 클릭하면 isOpen값이 반대로 바뀐다
+          >
+            👤
+          </button>
+          {isOpen && (
+            // isOpen이 true일때 다음내용을 보여줘라
+          <div className="dropdown">
+            <div className="dropdown-item">
+              <div>마이페이지</div>
+            </div>
+            <div className="dropdown-item">
+              <div>로그아웃</div>
+            </div>
+          </div>
+          )}
+        </div>
+      </div>
     </header>
   )
 }
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,25 @@
+
+function MyPage(){
+    return (
+        <div className="my-page-container">
+            <Header title="마이페이지" />
+            <div className="my-info">
+                내 정보
+            </div>
+            <div className="my-section">
+                담당구역
+            </div>
+            <div className="alert-setting">
+                알림 설정
+            </div>
+            <div className="my-info-change">
+                내 정보 변경
+            </div>
+            <div className="recent-work-logs">
+                최근 작업 로그
+            </div>
+        </div>
+    )
+}
+
+export default MyPage
