76afe299f99bd49456569b00d43b49ac77634a25
fff-commits <wgj21280@gmail.com>
2026-07-16 00:04:32 +0900

    상세 모니터링 프레임

--- src/pages/MonitoringDetailPage.jsx
+++ src/pages/MonitoringDetailPage.jsx
@@ -7,9 +7,26 @@ return(
     <div className="page-Layout">
         <div className="cctv-main-Layout">
             <div className="subCard">
-                <h4>
-                    main
-                </h4> 
+                <div className="cctv-main-Header">
+                    <span className="cctv-main-Live">
+                        Live
+                    </span>
+                    <div className="cctv-main-Info">
+                        <span>
+                            A동
+                        </span>
+                        <span>
+                            2층
+                        </span>
+                        <span>
+                            201-212 복도
+                        </span>
+                        <span>
+                            11:00:00
+                        </span>
+                    </div> 
+                </div>
+                
             </div>
             <div className="subCard">
                 <h1>CCTV</h1>
@@ -23,14 +40,37 @@ return(
         <div className="detection-main-Layout">
             <div className="detection-Section">
                 <div className="subCard">
-                    <h1>1</h1>
+                    <div className="detection-Object">
+                        <div className="detection-object-Title"> 
+                            탐지 객체
+                        </div>
+                    </div>
                 </div>
                 <div className="subCard">
-                    <h1>2</h1>
+                    <div className="detection-Object">
+                        <div className="detection-object-Title"> 
+                            이상 항목
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="subCard">
-                    <h1>3</h1>
+                <div className="detection-object-info-Section">
+                    <div className="detection-Object">
+                        <div className="detection-object-Title"> 
+                            선택 구격 탐지 정보
+                        </div>
+
+                        <div className="detection-object-info-button-Section">
+                            <button className="detection-Button detection-Button:hover">
+                                조치 요청
+                            </button>
+                            <button className="detection-Button detection-Button:hover">
+                                영상보기
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
