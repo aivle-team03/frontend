f233e03d8d516cbb4d11a8484c317c7cfd5d8fde
LimHHSS <abc96417@naver.com>
2026-07-22 14:54:46 +0900

    안전 교육/조치 이력 하위 탭 분리 작업, 교육 이수 관리 페이지와 교육 수강 페이지 분리, 헤더 작업

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,11 +3,13 @@ import MainLayout from '../components/layout/MainLayout.jsx'
 import ActionHistoryPage from '../pages/ActionHistoryPage.jsx'
 import ChecklistPage from '../pages/ChecklistPage.jsx'
 import EducationPage from '../pages/EducationPage.jsx'
+import EducationManagementPage from '../pages/EducationManagementPage.jsx'
 import HomePage from '../pages/HomePage.jsx'
 import LawQaPage from '../pages/LawQaPage.jsx'
 import MonitoringPage from '../pages/MonitoringPage.jsx'
 import MyPage from '../pages/MyPage.jsx'
 import MonitoringDetailPage from '../pages/MonitoringDetailPage.jsx'
+import RiskManagementPage from '../pages/RiskManagementPage.jsx'
 
 function AppRouter() {
   return (
@@ -20,6 +22,8 @@ function AppRouter() {
           <Route path="actions" element={<ActionHistoryPage />} />
           <Route path="law-qa" element={<LawQaPage />} />
           <Route path="education" element={<EducationPage />} />
+          <Route path="education-management" element={<EducationManagementPage />} />
+          <Route path="risk-management" element={<RiskManagementPage />} />
           <Route path="mypage" element={<MyPage />} />
           <Route path="monitoringdetail" element={<MonitoringDetailPage />} />
           <Route path="*" element={<Navigate replace to="/" />} />
