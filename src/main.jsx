0072426ed4f0472ba8887bcf1d46933057f81376
LimHHSS <abc96417@naver.com>
2026-07-29 17:08:27 +0900

    개인정보 처리방침, 이용약관 Footer 추가

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './styles/layout.css'
 import './styles/dashboard.css'
 import './styles/placeholder-page.css'
 import './styles/education.css'
+import './styles/service-footer.css'
+import './styles/policy-document.css'
 import App from './App.jsx'
 import theme from './theme/theme.js'
 
